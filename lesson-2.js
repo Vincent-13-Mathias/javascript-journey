1bd3f99529ade8d578918dbd1e2cc57b2c606e20
prometheus@13 <141336856+Vincent-13-Mathias@users.noreply.github.com>
2023-11-01 15:46:29 +0100

    Add files via upload

---
+++ lesson-2.js
@@ -0,0 +1,110 @@
+// users details
+const p = prompt('firstName');
+const q = prompt('lastname');
+const r = prompt('middle name');
+const s = prompt('Dob');
+const t = prompt('marital status');
+const u = prompt('weight');
+const v = prompt('height');
+const w = prompt('acadenical qualification');
+const x = prompt('identification number');
+
+ console.log(p);
+ console.log(q);
+ console.log(r);
+ console.log(s);
+ console.log(t);
+ console.log(u);
+ console.log(v);
+ console.log(w);
+ console.log(x);
+
+// object
+let ageClassification = {
+    firstName: 'Vincent',
+    lastName: 'Mathias',
+    middleName: 'Daniel',
+    dob: '25th-march-2004',
+    maritalStatus: 'single',
+    weight: '85kg',
+    height: '2m',
+    academicalQualification: 'secondary school',
+    identificationNumber: '55019'
+}
+
+console.log(ageClassification.firstName);
+console.log(ageClassification.lastName);
+console.log(ageClassification.middleName);
+console.log(ageClassification.dob);
+console.log(ageClassification.maritalStatus);
+console.log(ageClassification.weight);
+console.log(ageClassification.height);
+console.log(ageClassification.academicalQualification);
+console.log(ageClassification.identificationNumber);
+
+
+if (ageClassification =0-7){
+    console.log('toddler');
+}
+
+else {
+     console.log('adolescent');
+}
+
+if (ageClassification = 8-12){
+    console.log('adolescent');
+}
+
+else {
+     console.log('toddler');
+}
+
+if (ageClassification = 13-19){
+    console.log('teenager');
+}
+
+else {
+     console.log('adolescent');
+}
+
+if (ageClassification = 20-25){
+    console.log('baby adult');
+}
+
+else {
+     console.log('teenager');
+}
+
+if (ageClassification = 26-35){
+    console.log('adult');
+}
+
+else {
+     console.log('baby adult');
+}
+
+if (ageClassification = 36-55){
+    console.log('midlife crisis');
+}
+
+else {
+     console.log('adult');
+}
+
+if (ageClassification = 56){
+    console.log('ancestor');
+}
+
+else {
+     console.log('midlife crisis');
+}
+
+
+
+
+
+
+
+
+
+
